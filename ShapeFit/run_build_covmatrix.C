929b94d3213cc3803a85dd7378ba6cfd70b7820e
Matt Kramer <mkramer@lbl.gov>
2019-09-24 17:41:42 -0700

    Fix numbering/paths in run_build_covmatrix.C

--- ShapeFit/run_build_covmatrix.C
+++ ShapeFit/run_build_covmatrix.C
@@ -6,7 +6,7 @@ void run_build_covmatrix(int x = 1){
 
   Int_t i = x-1;
 
-  const Int_t nopts_sig = 9;
+  const Int_t nopts_sig = 11;
   TString options_sig[nopts_sig] = {
     "rel_escale", //0
     "scinti_nl",//1
@@ -16,24 +16,24 @@ void run_build_covmatrix(int x = 1){
     "core_spectra",//5
     "reac_power",//6
     "solar_oscpars",//7
-    "sigsys"//8
-    // "scinti_nl_corr_positron",//9
-    // "scinti_nl_corr_gamma",//10
+    "sigsys", //8
+    "scinti_nl_corr_positron",//9
+    "scinti_nl_corr_gamma",//10
   };
 
 
-  const Int_t nopts_bkg = 9;
+  const Int_t nopts_bkg = 10;
   TString options_bkg[nopts_bkg] = {
-    //"distort_aln", // 0
-    "distort_amc",
-    "distort_fn",
-    "distort_li9",
+    "distort_aln", // 0
+    "distort_amc", // 1
+    "distort_fn",  // 2
+    "distort_li9", // 3
     "vary_acc", // 4
-    "vary_aln",
-    "vary_amc",
-    "vary_fn",
-    "vary_li9",
-    "bgsys" // 9
+    "vary_aln", // 5
+    "vary_amc", // 6
+    "vary_fn",  // 7
+    "vary_li9", // 8
+    "bgsys", // 9
   };
   
   //for (Int_t i = 0; i < nopts_sig; i++){
@@ -41,8 +41,11 @@ void run_build_covmatrix(int x = 1){
     //TString toymc_filename = "../outputs/toySpectra_" + options_sig[i] + "_2017Model_p17b.root";
     //TString covmatrix_filename = "covariance_matrices/matrix_" + options_sig[i] + "_2017Model_P17B.txt";
       
-      TString toymc_filename = "../outputs/toySpectra_" + options_sig[i] + "_2017Model_p17b_0.2_inflated.root";
-      TString covmatrix_filename = "covariance_matrices/matrix_" + options_sig[i] + "_2017Model_P17B_0.2_inflated.txt";
+      // TString toymc_filename = "../outputs/toySpectra_" + options_sig[i] + "_2017Model_p17b_0.2_inflated.root";
+      // TString covmatrix_filename = "covariance_matrices/matrix_" + options_sig[i] + "_2017Model_P17B_0.2_inflated.txt";
+      
+    TString toymc_filename = "../outputs/toySpectra_" + options_sig[i] + ".root";
+    TString covmatrix_filename = "covariance_matrices/matrix_" + options_sig[i] + ".txt";
       
     cout << toymc_filename << "\t" << covmatrix_filename << endl;
     build_covmatrix(toymc_filename.Data(),covmatrix_filename.Data(),0);
@@ -53,12 +56,12 @@ void run_build_covmatrix(int x = 1){
     //TString toymc_filename = "../outputs/toySpectra_" + options_bkg[iii] + "_2017Model_p17b.root";
     //TString covmatrix_filename = "covariance_matrices/matrix_" + options_bkg[iii] + "_2017Model_P17B.txt";
       
-      TString toymc_filename = "../outputs/toySpectra_" + options_bkg[iii] + "_2017Model_p17b_0.2_inflated.root";
-      TString covmatrix_filename = "covariance_matrices/matrix_" + options_bkg[iii] + "_2017Model_P17B_0.2_inflated.txt";
+      // TString toymc_filename = "../outputs/toySpectra_" + options_bkg[iii] + "_2017Model_p17b_0.2_inflated.root";
+      // TString covmatrix_filename = "covariance_matrices/matrix_" + options_bkg[iii] + "_2017Model_P17B_0.2_inflated.txt";
       
-      
-    //TString toymc_filename = "../outputs/toySpectra/nominal/toySpectra_" + options_bkg[iii] + ".root";
-    // TString covmatrix_filename = "covariance_matrices/matrix_" + options_bkg[iii] + ".txt";
+    TString toymc_filename = "../outputs/toySpectra_" + options_bkg[iii] + ".root";
+    TString covmatrix_filename = "covariance_matrices/matrix_" + options_bkg[iii] + ".txt";
+
     cout << toymc_filename << "\t" << covmatrix_filename << endl;
     build_covmatrix(toymc_filename.Data(),covmatrix_filename.Data(),1);
   }
