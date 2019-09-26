f8a67e40b737ee950393b4114a87f6e8355bed95
Matt Kramer <mkramer@lbl.gov>
2019-09-26 00:23:38 -0700

    Add IHEP example; update install_example.sh

--- scripts/install_example.sh
+++ scripts/install_example.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-cp example/Theta13*.txt ShapeFit/Inputs
-cp example/*shapes*.root ShapeFit/Spectra
+tag=${1:-LBNL}; shift
+
+makelink() {
+    base=$1; shift
+    ext=$1; shift
+    dir=$1; shift
+
+    for stage in 6ad 8ad 7ad; do
+        ln -rs example/$tag/${base}_${stage}_*.$ext ShapeFit/$dir/${base}_$stage.$ext
+    done
+}
+
+makelink Theta13-inputs_P17B_inclusive txt Inputs
+makelink accidental_eprompt_shapes root Spectra
+makelink ibd_eprompt_shapes root Spectra
