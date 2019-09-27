5db82bf249c7dd729e424162582302ba9fd642bd
Matt Kramer <mkramer@lbl.gov>
2019-09-26 20:40:07 -0700

    Move unused code out of the way

--- ShapeFit/LoadClasses.C
+++ ShapeFit/LoadClasses.C
@@ -4,9 +4,9 @@
   gROOT->ProcessLine(".L PredSet.cc+");
   gROOT->ProcessLine(".L TimePeriodData.cc+");
   gROOT->ProcessLine(".L Predictor.cc+");
-  gROOT->ProcessLine(".L Ranger.cc+");
+  // gROOT->ProcessLine(".L Ranger.cc+");
   //gROOT->ProcessLine(".L ExtrapTable.cc+");
-  gROOT->ProcessLine(".L OscProbTable.cc+");
+  // gROOT->ProcessLine(".L OscProbTable.cc+");
   gROOT->ProcessLine(".L Binning.cc+");
   gROOT->ProcessLine(".L DataSet.cc+");
 }
