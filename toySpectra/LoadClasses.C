945a8960ce1a805d1d9350c1d8d3e3072952cd4e
Matt Kramer <mkramer@lbl.gov>
2020-03-18 14:58:16 -0700

    Obey $LBNL_FIT_DEBUG

--- toySpectra/LoadClasses.C
+++ toySpectra/LoadClasses.C
@@ -3,31 +3,33 @@
   TString include = ".include ";
   TString load = ".L ";
 
+  TString suffix = gSystem->Getenv("LBNL_FIT_DEBUG") ? "g" : "";
+
   TString prefix = "../ShapeFit";
   gROOT->ProcessLine( (include + prefix).Data() ); 
-  gROOT->ProcessLine( (load + prefix + "/OscCalc.cc+").Data() );
-  gROOT->ProcessLine( (load + prefix + "/FluxCalculator.cc+").Data() );
-  gROOT->ProcessLine( (load + prefix + "/PredSet.cc+").Data() );
-  gROOT->ProcessLine( (load + prefix + "/TimePeriodData.cc+").Data() );
-  gROOT->ProcessLine( (load + prefix + "/Predictor.cc+").Data() );
-  gROOT->ProcessLine( (load + prefix + "/Binning.cc+").Data() );
-  gROOT->ProcessLine( (load + prefix + "/DataSet.cc+").Data() );
+  gROOT->ProcessLine( (load + prefix + "/OscCalc.cc+" + suffix).Data() );
+  gROOT->ProcessLine( (load + prefix + "/FluxCalculator.cc+" + suffix).Data() );
+  gROOT->ProcessLine( (load + prefix + "/PredSet.cc+" + suffix).Data() );
+  gROOT->ProcessLine( (load + prefix + "/TimePeriodData.cc+" + suffix).Data() );
+  gROOT->ProcessLine( (load + prefix + "/Predictor.cc+" + suffix).Data() );
+  gROOT->ProcessLine( (load + prefix + "/Binning.cc+" + suffix).Data() );
+  gROOT->ProcessLine( (load + prefix + "/DataSet.cc+" + suffix).Data() );
 
   // TString prefix = "./quickFit";  
   // gROOT->ProcessLine( (include + prefix).Data() ); 
-  //gROOT->ProcessLine( (load + prefix + "/FitSummary.C+").Data() );
-  //gROOT->ProcessLine( (load + prefix + "/PhysicsModel.C+").Data() );
-  //gROOT->ProcessLine( (load + prefix + "/PoissonModel.C+").Data() );
-  //gROOT->ProcessLine( (load + prefix + "/QuickFitter.C+").Data() );
+  //gROOT->ProcessLine( (load + prefix + "/FitSummary.C+" + suffix).Data() );
+  //gROOT->ProcessLine( (load + prefix + "/PhysicsModel.C+" + suffix).Data() );
+  //gROOT->ProcessLine( (load + prefix + "/PoissonModel.C+" + suffix).Data() );
+  //gROOT->ProcessLine( (load + prefix + "/QuickFitter.C+" + suffix).Data() );
 
   TString prefix = "./reactor";
   gROOT->ProcessLine( (include + prefix).Data() ); 
-  gROOT->ProcessLine( (load + prefix + "/IsotopeTable.C+").Data() );
-  gROOT->ProcessLine( (load + prefix + "/CrossSectionTable.C+").Data() );
-  gROOT->ProcessLine( (load + prefix + "/CoreSpectrum.C+").Data() );
-  gROOT->ProcessLine( (load + prefix + "/Spectrum.C+").Data() );
-  //gROOT->ProcessLine( (load + prefix + "/ChiSquare.C+").Data() );
-  //gROOT->ProcessLine( (load + prefix + "/ChiSquareEnergyUnc.C+").Data() );
+  gROOT->ProcessLine( (load + prefix + "/IsotopeTable.C+" + suffix).Data() );
+  gROOT->ProcessLine( (load + prefix + "/CrossSectionTable.C+" + suffix).Data() );
+  gROOT->ProcessLine( (load + prefix + "/CoreSpectrum.C+" + suffix).Data() );
+  gROOT->ProcessLine( (load + prefix + "/Spectrum.C+" + suffix).Data() );
+  //gROOT->ProcessLine( (load + prefix + "/ChiSquare.C+" + suffix).Data() );
+  //gROOT->ProcessLine( (load + prefix + "/ChiSquareEnergyUnc.C+" + suffix).Data() );
 
   
  
