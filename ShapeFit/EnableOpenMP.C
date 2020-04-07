bb1ad39eb00bfc85555c38632c371df229a62426
Matt Kramer <mkramer@lbl.gov>
2020-04-07 01:31:07 -0700

    Fix silly memory leak in EnableOpenMP.C

--- ShapeFit/EnableOpenMP.C
+++ ShapeFit/EnableOpenMP.C
@@ -1,16 +1,17 @@
-TString arrstr(TObjArray& array, size_t i)
+TString arrstr(TObjArray* array, size_t i)
 {
-  return ((TObjString*)(array[i]))->GetString();
+  return ((TObjString*)(array->At(i)))->GetString();
 }
 
 void EnableOpenMP()
 {
   TString cmd = gSystem->GetMakeSharedLib();
-  TObjArray& parts = *cmd.Tokenize(";");
+  TObjArray* parts = cmd.Tokenize(";");
   TString cd = arrstr(parts, 0);
   TString compile = arrstr(parts, 1);
   TString link = arrstr(parts, 2);
   const char* newCmd = Form("%s ; %s -fopenmp ; %s -fopenmp",
                             cd.Data(), compile.Data(), link.Data());
   gSystem->SetMakeSharedLib(newCmd);
+  delete parts;
 }
