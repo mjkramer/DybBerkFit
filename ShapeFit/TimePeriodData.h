4c755fb34aa15d1a40345e75b4981dbcd79c284c
Matt Kramer <mkramer@lbl.gov>
2020-04-09 01:10:36 -0700

    Zero-initialize TimePeriodData
    Specifically for the sake of the line "if (p) delete p;" in
    Predictor::LoadToyMCEntry, but also a good idea in general.

--- ShapeFit/TimePeriodData.h
+++ ShapeFit/TimePeriodData.h
@@ -18,37 +18,37 @@ public:
   void CorrectSpec(bool print=false);//<---correct for bg, livetimes and muon efficiencies
   void PrintToScreen();
 
-  Double_t Livetime[Ndetectors];//<--in days? (doesn't matter as long as use same units)
-  Double_t MuonVetoEff[Ndetectors];//<--muon veto efficiency (percent of time that did not cut out?)
-  Double_t DMCEff[Ndetectors];
-  Double_t ObsEvts[Ndetectors];
-  Double_t ErrEvts[Ndetectors];
-  Double_t CorrEvts[Ndetectors];
-  Double_t BgEvts[Ndetectors];
-  Double_t AccEvts[Ndetectors];
-  Double_t Li9Evts[Ndetectors];
-  Double_t AmcEvts[Ndetectors];
-  Double_t AlnEvts[Ndetectors];
-  Double_t FnEvts[Ndetectors];
-  Double_t AccErr[Ndetectors];
-  Double_t Li9Err[Ndetectors];
-  Double_t AmcErr[Ndetectors];
-  Double_t AlnErr[Ndetectors];
-  Double_t FnErr[Ndetectors];
-  Double_t CorrBgEvts[Ndetectors];
-  Double_t ErrBgEvts[Ndetectors]; // statistical uncertainties for the backgrounds
-  Double_t TargetMass[Ndetectors];
-  Double_t BgEvtsLiv[Ndetectors];
+  Double_t Livetime[Ndetectors] = {};//<--in days? (doesn't matter as long as use same units)
+  Double_t MuonVetoEff[Ndetectors] = {};//<--muon veto efficiency (percent of time that did not cut out?)
+  Double_t DMCEff[Ndetectors] = {};
+  Double_t ObsEvts[Ndetectors] = {};
+  Double_t ErrEvts[Ndetectors] = {};
+  Double_t CorrEvts[Ndetectors] = {};
+  Double_t BgEvts[Ndetectors] = {};
+  Double_t AccEvts[Ndetectors] = {};
+  Double_t Li9Evts[Ndetectors] = {};
+  Double_t AmcEvts[Ndetectors] = {};
+  Double_t AlnEvts[Ndetectors] = {};
+  Double_t FnEvts[Ndetectors] = {};
+  Double_t AccErr[Ndetectors] = {};
+  Double_t Li9Err[Ndetectors] = {};
+  Double_t AmcErr[Ndetectors] = {};
+  Double_t AlnErr[Ndetectors] = {};
+  Double_t FnErr[Ndetectors] = {};
+  Double_t CorrBgEvts[Ndetectors] = {};
+  Double_t ErrBgEvts[Ndetectors] = {}; // statistical uncertainties for the backgrounds
+  Double_t TargetMass[Ndetectors] = {};
+  Double_t BgEvtsLiv[Ndetectors] = {};
 
   //Note to self: do not define as many histograms for bg as have equivalents for numbers above for simplicity and in order to not have to clone them a lot.
-  TH1F *ObsEvtsSpec[Ndetectors];
-  TH1F *CorrBgEvtsSpec[Ndetectors];
-  TH1F *CorrAccEvtsSpec[Ndetectors];
-  TH1F *CorrLi9EvtsSpec[Ndetectors];
-  TH1F *CorrAmcEvtsSpec[Ndetectors];
-  TH1F *CorrFnEvtsSpec[Ndetectors];
-  TH1F *CorrAlnEvtsSpec[Ndetectors];
-  TH1F *CorrEvtsSpec[Ndetectors];
+  TH1F *ObsEvtsSpec[Ndetectors] = {};
+  TH1F *CorrBgEvtsSpec[Ndetectors] = {};
+  TH1F *CorrAccEvtsSpec[Ndetectors] = {};
+  TH1F *CorrLi9EvtsSpec[Ndetectors] = {};
+  TH1F *CorrAmcEvtsSpec[Ndetectors] = {};
+  TH1F *CorrFnEvtsSpec[Ndetectors] = {};
+  TH1F *CorrAlnEvtsSpec[Ndetectors] = {};
+  TH1F *CorrEvtsSpec[Ndetectors] = {};
 
 private:
   bool firstcorrection;
