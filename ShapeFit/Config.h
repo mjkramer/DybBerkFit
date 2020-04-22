9e29c08520f426ee9bebb741aab88a876c4e64e5
Matt Kramer <mkramer@lbl.gov>
2020-04-21 19:29:42 -0700

    Move constants from FluxCalculator.h to Config.h

--- ShapeFit/Config.h
+++ ShapeFit/Config.h
@@ -6,8 +6,22 @@
 
 namespace Config {
 
+const int Ncores = 6;
+const int Ndetectors = 8;
+const int Nhalls = 3;
+const int Nstage = 3;
+
 const bool useBcwBinning = false;
 const double lowBinInflation = 0; // was 0.1 originally
+
+const int MaxPredictions = 16; // 4 near AD predicting 4 far AD: 4 x 4 = 16
+const int detConfigEH[Ndetectors] = {1, 1, 2, 2, 3, 3, 3, 3}; // EH
+const int detConfigAD[Ndetectors] = {1, 2, 1, 2, 1, 2, 3, 4}; // AD
+
+// This part needs to be modified when the detector config changes
+const int NdetectorsConfig[3][Ndetectors] = {{1, 1, 1, 0, 1, 1, 1, 0},
+                                             {1, 1, 1, 1, 1, 1, 1, 1},
+                                             {0, 1, 1, 1, 1, 1, 1, 1}};
 
 // chi2 map binning
 // const Int_t nsteps = 101;
