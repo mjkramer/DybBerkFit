145701cb188d9d50fdb77ca58a5ca77eda57147a
Matt Kramer <mkramer@lbl.gov>
2020-04-23 02:28:21 -0700

    Make install_inputs.sh copy, not symlink
    --remove-destination ensures that we replace any symlinks

--- scripts/install_inputs.sh
+++ scripts/install_inputs.sh
@@ -7,16 +7,16 @@ if [ -z "$rootdir" ]; then
     exit 1
 fi
 
-makelink() {
+copy() {
     base=$1; shift
     ext=$1; shift
     dir=$1; shift
 
     for stage in 6ad 8ad 7ad; do
-        ln -rsf $rootdir/${base}_${stage}*.$ext ShapeFit/$dir/${base}_$stage.$ext
+        cp --remove-destination $rootdir/${base}_${stage}*.$ext ShapeFit/$dir/${base}_$stage.$ext
     done
 }
 
-makelink Theta13-inputs_P17B_inclusive txt Inputs
-makelink accidental_eprompt_shapes root Spectra
-makelink ibd_eprompt_shapes root Spectra
+copy Theta13-inputs_P17B_inclusive txt Inputs
+copy accidental_eprompt_shapes root Spectra
+copy ibd_eprompt_shapes root Spectra
