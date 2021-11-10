bdabe1689ddb775689572c37b23715107deea458
Matt Kramer <mkramer@lbl.gov>
2021-11-09 20:07:11 -0800

    Switch to LBNL isotope spectra

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,7 +5,7 @@ BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
 # -------------------------- Generate reactor spectra --------------------------
 genReactor() {
     cd $BASE/ReactorPowerCalculator
-    root -b -q "Produce_Isotope_SpectraP17B_unblinded.C(1)"
+    root -b -q "Produce_Isotope_SpectraP17B_unblinded.C(4)"
     cd isotope_spectra_by_Beda
     root -b -q make_combined_spectra_P17B_unblinded.C
 }
