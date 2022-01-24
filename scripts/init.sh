cd3012f79579eef75afddecbc6c95cecb60bc682
Matt Kramer <matt314159@gmail.com>
2022-01-24 10:15:24 -0800

    Update init.sh for blinded full data set

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,9 +5,11 @@ BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
 # -------------------------- Generate reactor spectra --------------------------
 genReactor() {
     cd $BASE/ReactorPowerCalculator
-    root -b -q "Produce_Isotope_SpectraP17B_unblinded.C(1)"
-    cd isotope_spectra_by_Beda
-    root -b -q make_combined_spectra_P17B_unblinded.C
+    # root -b -q "Produce_Isotope_SpectraP17B_unblinded.C(1)"
+    ./prod_isotope_spec.py
+    # cd isotope_spectra_by_Beda
+    cd isotope_spectra_v4v5v3v1_blinded
+    root -b -q make_combined_spectra_v4v5v3v1_blinded.C
 }
 
 # ------------------------ Generate ToyMC config files -------------------------
